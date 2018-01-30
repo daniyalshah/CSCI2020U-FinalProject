1e73096267e49a2a9fe09ebb25367b0df4264139
mustafalazzawe <mustafa.alazzawe@gmail.com>
2018-01-30 14:30:48 -0500

    final test

--- lab01/Lab01Hello.java
+++ lab01/Lab01Hello.java
@@ -1,7 +1,7 @@
 public class Lab01Hello{
   public static void main(String[] args){
 <<<<<<< HEAD
-    System.out.println("Lab01, Hello! this is an update loool");
+    System.out.println("Lab01, Hello! this is an update loool, byeee");
 =======
     System.out.println("Lab01, Hello! this is in beta");
 >>>>>>> beta
