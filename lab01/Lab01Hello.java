eb5281c4c47e8e38fa875d742e36f30efb567d4c
mustafalazzawe <mustafa.alazzawe@gmail.com>
2018-04-02 20:50:26 -0400

    removing lab01 folder

--- lab01/Lab01Hello.java
+++
@@ -1,9 +0,0 @@
-public class Lab01Hello{
-  public static void main(String[] args){
-<<<<<<< HEAD
-    System.out.println("Lab01, Hello! this is an update loool, byeee");
-=======
-    System.out.println("Lab01, Hello! this is in beta");
->>>>>>> beta
-  }
-}
